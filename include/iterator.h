4bcfcc9240ff6190e06fdc3674ca3c19e2c84c83
root <root@Home>
2024-03-23 20:55:31 +0300

    added iterators

--- include/iterator.h
+++ include/iterator.h
@@ -3,9 +3,17 @@
 
 #include "deque.h"
 
-typedef struct Iterator {
-	Node*
+#define STRUCT_DATA_TYPE Node
+#define STRUCT_TYPE Deque
 
+typedef struct Iterator {
+	STRUCT_DATA_TYPE* node;
 } Iterator;
+
+Iterator* iter_create(STRUCT_TYPE*);
+STRUCT_DATA_TYPE* iter_next(Iterator* iter);
+void iter_set(Iterator* iter, VALUE_TYPE);
+VALUE_TYPE iter_get(Iterator* iter);
+void iter_delete(Iterator* iter);
 
 #endif
---
+++ src/iterator.c
@@ -0,0 +1,14 @@
+#include "iterator.h"
+
+
+Iterator* iter_create(STRUCT_TYPE* struct_object) {
+	Iterator* iter = (Iterator *) malloc(sizeof(Iterator));
+	iter->node = struct_object->left;
+}
+
+
+
+STRUCT_DATA_TYPE* iter_next(Iterator* iter);
+void iter_set(Iterator* iter, VALUE_TYPE);
+VALUE_TYPE iter_get(Iterator* iter);
+void iter_delete(Iterator* iter);
